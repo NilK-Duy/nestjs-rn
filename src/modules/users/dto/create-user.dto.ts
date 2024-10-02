7412a5e6f17b716bbdc78d7bd76c0690e7d53cdf
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-10-02 16:50:48 +0700

    crud user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: "Name should not be empty" })
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail({}, { message: "Invalid email" })
+  email: string;
+
+  @IsNotEmpty({ message: "Password should not be empty" })
+  password: string;
+
+  phone: string;
+  address: string;
+  image: string;
+
+}
