0e8174dc1c93ddf07d8d5f28e90efe5444abc112
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-10-03 12:07:53 +0700

    send email with template

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtAuthGuard } from './passport/jwt-auth.guard';
 import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService
+  ) {}
 
   @Post('login')
   @Public()
@@ -18,8 +21,25 @@ export class AuthController {
 
   @Post('register')
   @Public()
-  register (@Body() registerDto: CreateAuthDto) {
+  register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
   }
 
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService
+    .sendMail({
+      to: 'nguyenduyxtkd@gmail.com', // list of receivers
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      template: 'register',
+      context: {
+        name: 'Ryan',
+        activationCode: 123456789
+      }
+    })
+    return 'ok';
+  }
+  
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -10,12 +10,15 @@ import mongoose from 'mongoose';
 import { CreateAuthDto } from '@/auth/dto/create-auth.dto';
 import { v4 as uuidv4 } from 'uuid';
 import dayjs from 'dayjs';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+
+    private readonly mailerService: MailerService
 
   ) {}
 
@@ -100,19 +103,30 @@ export class UsersService {
 
     //hash password
     const hashPassword = await hashPasswordHelper(password);
+    const coideId = uuidv4();
     const user = await this.userModel.create({
       name, email, password: hashPassword,
       isActive: false,
-      codeId: uuidv4(),
-      codeExpired: dayjs().add(1, 'day')
+      codeId: coideId,
+      // codeExpired: dayjs().add(5, 'minutes')
+      codeExpired: dayjs().add(30, 'seconds')
+    })
+    
+    //send email
+    this.mailerService.sendMail({
+      to: user.email, // list of receivers
+      subject: 'Activate your account at @ryan', // Subject line
+      template: 'register',
+      context: {
+        name: user?.name ?? user.email,
+        activationCode: coideId
+      }
     })
 
     //send feedback
     return {
       _id: user._id
     }
-
-    //send email
 
   }
 }
